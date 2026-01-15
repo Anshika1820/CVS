f03ef616c5be534985b80108357406266682c64f
Anshika Srivastava <srivastavaanshika510@gmail.com>
2026-01-15 17:38:27 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,24 +13,52 @@ const Home = () => {
       <Navbar />
 
       {/* Main Layout */}
-      <div className="grid grid-cols-12 gap-6 px-6 py-6 bg-black text-white">
+      <div className="
+        grid 
+        grid-cols-1 
+        md:grid-cols-12 
+        gap-4 
+        md:gap-6 
+        px-3 
+        md:px-6 
+        py-4 
+        md:py-6 
+        bg-black 
+        text-white
+        overflow-x-hidden
+      ">
 
         {/* LEFT SIDEBAR */}
-        <div className="col-span-3 space-y-6">
+        <div className="
+          col-span-12 
+          md:col-span-4 
+          lg:col-span-3 
+          space-y-6
+        ">
           <Ask />
           <PopularTopics />
           <TrendingQuestions />
         </div>
 
         {/* FEED */}
-        <div className="col-span-6 space-y-4">
+        <div className="
+          col-span-12 
+          md:col-span-8 
+          lg:col-span-6 
+          space-y-4
+        ">
           <FeedCard />
           <FeedCard />
           <FeedCard />
         </div>
 
         {/* RIGHT SIDEBAR */}
-        <div className="col-span-3 space-y-6">
+        <div className="
+          hidden 
+          lg:block 
+          lg:col-span-3 
+          space-y-6
+        ">
           <Sidebar />
           <WhoToFollow />
         </div>
@@ -38,7 +66,7 @@ const Home = () => {
       </div>
 
       {/* Footer */}
-      <div className="border-t border-green-500 mt-10">
+      <div className="border-t border-green-500 mt-6 md:mt-10">
         <Footer />
       </div>
     </>
